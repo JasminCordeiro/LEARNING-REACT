9ed420b996536ec2f61019f07fa11cd7e938498a
Jasmin Cordeiro <playgame0159@gmail.com>
2024-02-27 23:54:03 -0300

    Updates

--- 1/meu-projeto/src/App.js
+++ 1/meu-projeto/src/App.js
@@ -5,11 +5,20 @@ function App() {
 
   const name = 'Jasmin'
 
+  function sum(a,b){
+    return a + b
+  }
+  const url = 'https://via.placeholder.com/150'
+
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <p>Olá {name}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt = 'Imagem' />
+
+      
       
     </div>
     
