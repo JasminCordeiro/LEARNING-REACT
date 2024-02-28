b4f366d43e98dd1d2d9fea3f802b3a16f9bfebd2
Jasmin Cordeiro <playgame0159@gmail.com>
2024-02-27 23:41:24 -0300

    Updates

---
+++ 1/meu-projeto/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = 'Jasmin'
+
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Olá {name}</p>
+      
+    </div>
+    
+  );
+}
+
+export default App;
