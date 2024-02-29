ee7f51139d15284deaa8b293f2d004542dd91621
Jasmin Cordeiro <playgame0159@gmail.com>
2024-02-28 23:17:13 -0300

    Updates

--- 1/meu-projeto/src/App.js
+++ 1/meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         Profissão="Dev Junior" 
         foto="https://via.placeholder.com/150"/>
         <Frase/>
+        <List/>
 
 
     </div>
